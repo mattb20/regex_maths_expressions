1a178568f4cf1627ba5918726fd84df44895fd4d
mattb20 <matt_burgoyne@live.co.uk>
2018-06-14 14:00:37 +0100

    all fractions finished

--- lib/string.rb
+++ lib/string.rb
@@ -3,9 +3,18 @@ class String
     if /frac{-?[0-9]+}{-?[0-9]+}/.match(self)
       args = self.scan(/-?[0-9]+/).map(&:to_i)
       div(args)
-    elsif /frac{-?[0-9]*[a-zA-Z]+}{-?[0-9]*[a-zA-Z]+}/.match(self)
-      args = self.scan(/-?[0-9]*[a-zA-Z]+/) - ['frac']
-      div(args)
+    elsif /frac{-?[0-9]*[a-zA-Z]*}{-?[0-9]*[a-zA-Z]*}/.match(self)
+      string_args = (self.scan(/-?[0-9]*[a-zA-Z]*/) - ['frac']).reject do |args_with_blank|
+        args_with_blank.empty?
+      end
+      string_args.each_with_index do |arg, i|
+        if /[0-9]+/.match(arg)
+          string_args[i] = arg.to_i
+        else
+          string_args[i] = arg
+        end
+      end
+      div(string_args)
 
     end
   end
